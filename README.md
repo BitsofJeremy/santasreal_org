18a49caefdbc2a78fdc82b039381d4f046c51139
abvavgjeremy <hello@funinternetthings.com>
2023-05-04 12:06:12 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# santasreal_org
-Flask site for santasreal.org
+# Santa's Real Flask App
+
+View live:  [santasreal](https://santasreal.org/)
+
+# Install
+
+    git clone https://github.com/ephergent/santasreal_org.git
+    cd santasreal_org
+    virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+*[Note: See an example .env file below]*
+
+    source .env
+
+### Run it
+
+    python app.py
+
+
+Open `http://127.0.0.1:5000/` in browser of choice.
+
+
+#### Example .env file
+
+    # Mailgun Config
+    export MAILGUN_API_KEY='API KEY'
+    export DOMAIN_NAME='EMAIL DOMAIN'
+    export ADMIN_EMAIL='ADMIN EMAIL'
+
+    # Basic Flask stuff
+    export FLASK_APP=app.py
+    export FLASK_CONFIG=development
+    export SALTY_SECRET=bbq
+    export CSRF_SESSION_KEY=hotdogs
+    export SECRET_KEY=pizza
---
+++ nftgallery/home/views.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, render_template, request, flash
+import os
+import requests
+import logging
+
+MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
+DOMAIN_NAME = os.getenv('DOMAIN_NAME')
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
+
+logger = logging.getLogger(__name__)
+
+home = Blueprint('home', __name__)
+
+
+def send_simple_message(_email="None@NoneYouBusiness.com", _text=None):
+    return requests.post(
+        f"https://api.mailgun.net/v3/{DOMAIN_NAME}/messages",
+        auth=("api", MAILGUN_API_KEY),
+        data={"from": _email,
+              "to": [ADMIN_EMAIL],
+              "subject": "SantasReal: Message",
+              "text": _text})
+
+
+# Basic site routes
+@home.route('/', methods=['GET'])
+def index_get():
+    """ Just a GET request """
+    return render_template('index.html')
+
+
+@home.route('/', methods=['POST'])
+def index_post():
+    """ POST request """
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    # flash(f"Name: {name} \n Email: {email} \n Message: {message}")
+    sent = send_simple_message(
+        _email=email,
+        _text=f"Name: {name} \n\nEmail: {email} \n\nMessage: {message}"
+    )
+    if sent.status_code == 200:
+        flash("Message Sent!")
+    else:
+        flash("Sending Message Failed. :( ")
+    return render_template('index.html')
